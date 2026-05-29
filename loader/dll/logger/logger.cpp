74ea88f9a9169212fc71c739b9bf0e9f97e792f9
DucksAreVeryCoolDev <lucasgrant2020@outlook.com>
2026-05-29 16:12:55 +0100

    Update logger.cpp

--- loader/dll/logger/logger.cpp
+++ loader/dll/logger/logger.cpp
@@ -2,8 +2,32 @@
 
 #include <Windows.h>
 #include <iostream>
+#include <cstdio>
 
 namespace Logger
 {
-	
+	void Init()
+	{
+		AllocConsole();
+
+		FILE* file;
+		freopen_s(&file, "CONOUT$", "w", stdout);
+
+		SetConsoleTitleA("Rock SDK | Logger");
+	}
+
+	void Info(const std::string& message)
+	{
+		std::cout << "[INFO] " << message << std::endl;
+	}
+
+	void Warn(const std::string& message)
+	{
+		std::cout << "[INFO] " << message << std::endl;
+	}
+
+	void Error(const std::string& message)
+	{
+		std::cout << "[INFO] " << message << std::endl;
+	}
 }
