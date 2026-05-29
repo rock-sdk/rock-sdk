ee889c228835179348712591d3174901a6edcd1b
Lumi <160318758+LumiMakesStuff@users.noreply.github.com>
2026-05-29 15:44:06 +0100

    [FEATURE] Threaded init + 3 Markdowns (#2)
    ## Changes
    - Separate init thread | Runs alongside Minecraft
    - Won't block game startup
    - Disabled thread library callbacks
    - Work-in-progress CONTRIBUTING.md
    - Add building with CMake into README.md
    - Add start of Reverse Engineering Notes for 1.26.21.01
    
    ## Notes
    - Kept Rock SDK Loaded popup for testing | Will be replaced with logs
    soon
    - In future Features such as the RE notes and CONTRIBUTING.md will be in
    separate branches/PRs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-sdk
+
+
+## Building (Visual Studio 2022)
+- Right click root (rock-sdk/)
+- Open in terminal
+- Run ```cmake -B build```
+- Run ```cmake --build build --config Release``` for release or ```cmake --build build --config Debug``` for debug
+- The DLL should be in rock-sdk/build/loader/dll/Release/rockloader.dll
--- loader/dll/main.cpp
+++ loader/dll/main.cpp
@@ -1,14 +1,30 @@
 #include <Windows.h>
 
+DWORD WINAPI InitThread(LPVOID)
+{
+	MessageBoxA(
+		nullptr,
+		"Rock SDK loaded!",
+		"Rock SDK",
+		MB_OK
+	);
+
+	return 0;
+}
+
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved)
 {
 	if (reason == DLL_PROCESS_ATTACH)
 	{
-		MessageBoxA(
+		DisableThreadLibraryCalls(module);
+
+		CreateThread(
 			nullptr,
-			"Rock SDK loaded!",
-			"rock-sdk",
-			MB_OK
+			0,
+			InitThread,
+			nullptr,
+			0,
+			nullptr
 		);
 	}
 
